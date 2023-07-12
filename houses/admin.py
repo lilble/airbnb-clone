b89d10d4e48fe0c72f8106c96ff9e08d0cf0cfa9
lilble <yerimnemo@gmail.com>
2023-07-12 14:23:33 -0700

    #4 done

--- houses/admin.py
+++ houses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import House
 
 # Register your models here.
+
+@admin.register(House)  # decorator, admin 패널에 'House'라는 모델을 등록하고 싶다는 뜻
+class HouseAdmin(admin.ModelAdmin):
+    list_display = ("name", "price_per_month", "address", "pets_allowed")
+    list_filter = ("price_per_month", "pets_allowed")
+    search_fields = ("address",)
--- houses/models.py
+++ houses/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class House(models.Model):  # models.Model 상속.
+
+    """Model Definition of Houses""" ## model에 대한 설명을 써주는 관행
+
+    name = models.CharField(max_length=140, unique=True)
+    price_per_month = models.PositiveBigIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True, help_text="Does the house allow pets?")
+
+    def __str__(self):
+        return self.name
